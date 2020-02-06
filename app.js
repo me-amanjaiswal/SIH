b4da6115f03de6020af7ed0199e1c3599389a293
Aman jaiswal <aman.ja943@gmail.com>
2020-02-06 20:29:27 +0530

    up1

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+var port=3000;
+var router = express.Router();
+
+  
+var path = __dirname + "/views/";
+app.use(express.static('public'));
+  
+app.use("/",router);
+  
+router.get("/",function(req, res){
+
+  res.sendFile(path + "home.html");
+});
+
+router.get("/login",function(req, res){
+  res.sendFile(path + "login.html");
+});
+router.get("/About",function(req, res){
+
+  res.sendFile(path + "about.html");
+});
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
